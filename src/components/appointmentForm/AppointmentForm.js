22af52791e3a2e7adecde335bf4a6b358bcfb29b
Edgar Flauzino <edgarflauzino@gmail.com>
2023-02-14 23:37:56 -0300

    first commit

---
+++ src/components/appointmentForm/AppointmentForm.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { ContactPicker } from "../../components/contactPicker/ContactPicker";
+
+export const AppointmentForm = ({
+  contacts,
+  title,
+  setTitle,
+  contact,
+  setContact,
+  date,
+  setDate,
+  time,
+  setTime,
+  handleSubmit
+}) => {
+  const getTodayString = () => {
+    const [month, day, year] = new Date()
+      .toLocaleDateString("en-US")
+      .split("/");
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  };
+
+  const getContactNames = () => {
+    return contacts.map((contact) => contact.name);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type='text'
+          name='title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder='Appointment title'
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type='text'
+          name='date'
+          min={getTodayString()}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          required
+          placeholder='Appointment date'
+        />
+      </label>
+      <br />
+      <label>
+        <input
+          type='text'
+          name='time'
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          required
+          placeholder='Appointment time'
+        />
+      </label>
+      <br />
+      <label>
+        <ContactPicker
+          name="contact"
+          value={contact}
+          contacts={getContactNames()}
+          onChange={(e) => setContact(e.target.value)}
+          placeholder="Appointment With"
+        />
+      </label>
+      <br />
+      <input type='submit' name='Add Appointment' />
+    </form>
+  );
+};
